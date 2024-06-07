e53173c0eb18f676be9b8747f9aae0c63a9f69b0
MD ALAMIN KAZI <kmalamin123210@gmail.com>
2024-06-07 16:13:40 +0600

    added apllication form

--- src/components/buttons/PrimaryButton.jsx
+++ src/components/buttons/PrimaryButton.jsx
@@ -1,7 +1,7 @@
 import { FaCircleArrowRight } from "react-icons/fa6";
 const PrimaryButton = ({ text }) => {
   return (
-    <button className="px-10 py-4 bg-[#1A73E8] text-white text-2xl font-bold rounded-xl flex items-center gap-4">
+    <button className="px-10 py-4 bg-[#1A73E8] text-white text-2xl font-bold rounded-xl flex justify-center items-center gap-4">
       {text} <FaCircleArrowRight />
     </button>
   );
--- src/pages/Payment/CheckoutForm.jsx
+++ src/pages/Payment/CheckoutForm.jsx
@@ -3,11 +3,14 @@ import { useContext, useEffect, useState } from "react";
 import useAxiosSecure from "../../hooks/useAxiosSecure";
 import { AuthContext } from "../../context/AuthProvider";
 import { toast } from "react-toastify";
+import { useForm } from "react-hook-form";
+import PrimaryButton from "../../components/buttons/PrimaryButton";
 
 const CheckoutForm = ({ fee, id }) => {
   const [error, setError] = useState("");
   const [tnxid, setTnxid] = useState("");
   const [clientSecret, setClientSecret] = useState("");
+  const [isPaymentSuccessful, setIsPaymentSuccessful] = useState(false);
   const { user } = useContext(AuthContext);
   const stripe = useStripe();
   const elements = useElements();
@@ -16,64 +19,53 @@ const CheckoutForm = ({ fee, id }) => {
   useEffect(() => {
     if (fee > 0) {
       axiosSecure.post("/create-payment-intent", { price: fee }).then((res) => {
-        console.log(res.data.clientSecret);
         setClientSecret(res.data.clientSecret);
       });
     }
   }, [axiosSecure, fee]);
 
-  const handleSubmit = async (event) => {
+  const handlePaymentSubmit = async (event) => {
     event.preventDefault();
 
     if (!stripe || !elements) {
-      // Stripe.js has not loaded yet. Make sure to disable
-      // form submission until Stripe.js has loaded.
       return;
     }
 
-    // Get a reference to a mounted CardElement. Elements knows how
-    // to find your CardElement because there can only ever be one of
-    // each type of element.
     const card = elements.getElement(CardElement);
 
     if (card == null) {
       return;
     }
 
-    // Use your card Element with other Stripe.js APIs
     const { error, paymentMethod } = await stripe.createPaymentMethod({
       type: "card",
       card,
     });
 
     if (error) {
-      console.log("[error]", error);
       setError(error.message);
     } else {
-      console.log("[PaymentMethod]", paymentMethod);
       setError("");
     }
 
-    // confirm payment
     const { paymentIntent, error: confirmError } =
       await stripe.confirmCardPayment(clientSecret, {
         payment_method: {
           card: card,
           billing_details: {
-            email: user?.email || "anonymouse",
-            name: user?.displayName || "anonymouse",
+            email: user?.email || "anonymous",
+            name: user?.displayName || "anonymous",
           },
         },
       });
 
     if (confirmError) {
-      console.log("Confirm Error");
+      setError(confirmError.message);
     } else {
-      console.log("Payment intent, ", paymentIntent);
       if (paymentIntent.status === "succeeded") {
         setTnxid(paymentIntent.id);
+        setIsPaymentSuccessful(true);
 
-        // now save the paymnet to the database
         const payment = {
           email: user.email,
           price: fee,
@@ -84,51 +76,216 @@ const CheckoutForm = ({ fee, id }) => {
         };
 
         const res = await axiosSecure.post("/payments", payment);
-        console.log("Paymnet saved", res.data);
         if (res.data.insertedId) {
-          toast.success("Payment Successfull");
+          toast.success("Payment Successful");
         }
       }
     }
   };
-  return (
-    <form
-      className=" md:w-[500px] shadow-xl flex flex-col mx-auto space-y-10 mt-10 border-2 p-6 rounded-xl"
-      onSubmit={handleSubmit}
-    >
-      <CardElement
-        options={{
-          style: {
-            base: {
-              fontSize: "16px",
-              color: "#424770",
-              "::placeholder": {
-                color: "#aab7c4",
-              },
-            },
-            invalid: {
-              color: "#9e2146",
-            },
-          },
-        }}
-      />
 
-      <button
-        className="btn btn-primary"
-        type="submit"
-        disabled={!stripe || !clientSecret}
-      >
-        $ {fee}
-      </button>
-      <div>
-        <p className="text-red-700 text-center">{error}</p>
-        {tnxid && (
-          <p className="text-green-700 text-center">
-            Your Transaction Id: {tnxid}
-          </p>
-        )}
-      </div>
-    </form>
+  //   application form
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data);
+  console.log(errors);
+
+  return (
+    <div>
+      {!isPaymentSuccessful && (
+        <form
+          className="md:w-[500px] shadow-xl flex flex-col mx-auto space-y-10 mt-10 border-2 p-6 rounded-xl"
+          onSubmit={handlePaymentSubmit}
+        >
+          <CardElement
+            options={{
+              style: {
+                base: {
+                  fontSize: "16px",
+                  color: "#424770",
+                  "::placeholder": {
+                    color: "#aab7c4",
+                  },
+                },
+                invalid: {
+                  color: "#9e2146",
+                },
+              },
+            }}
+          />
+          <button
+            className="btn btn-primary"
+            type="submit"
+            disabled={!stripe || !clientSecret}
+          >
+            Pay ${fee}
+          </button>
+          <div>
+            <p className="text-red-700 text-center">{error}</p>
+            {tnxid && (
+              <p className="text-green-700 text-center">
+                Your Transaction Id: {tnxid}
+              </p>
+            )}
+          </div>
+        </form>
+      )}
+
+      {isPaymentSuccessful && (
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="mt-10 md:w-[660px] shadow-xl flex flex-col mx-auto space-y-10  border-2 p-6 rounded-xl "
+        >
+          <h2 className="text-center text-2xl mb-4 uppercase font-bold">
+            Applicant Information
+          </h2>
+
+          <div className=" space-y-2">
+            <label className="font-bold">Phone Number*</label>
+            <input
+              {...register("phone", {
+                required: true,
+                minLength: 11,
+                maxLength: 15,
+              })}
+              type="number"
+              placeholder="Your Phone Number"
+              className="input input-bordered w-full"
+            />
+          </div>
+
+          <div className="flex flex-col md:flex-row justify-between items-center">
+            <div className=" space-y-2">
+              <label className="font-bold">Your Photo*</label>
+              <input
+                {...register("photo", { required: true })}
+                type="file"
+                placeholder="Upload your photo"
+                className="file-input w-full"
+              />
+            </div>
+            <div className=" space-y-2">
+              <label className="font-bold">Gender*</label>
+              <br />
+              <select
+                {...register("gender", { required: true })}
+                className="select select-bordered w-full"
+              >
+                <option disabled selected>
+                  Select Your Gender
+                </option>
+                <option value="female">Female</option>
+                <option value="male">Male</option>
+              </select>
+            </div>
+          </div>
+
+          <div className=" space-y-2">
+            <label className="font-bold">Your Address*</label>
+            <input
+              {...register("address", { required: true })}
+              type="text"
+              placeholder="Your Address : Village, District, Country"
+              className="input input-bordered w-full"
+            />
+          </div>
+
+          <div className="flex flex-col md:flex-row justify-between items-center">
+            <div className=" space-y-2">
+              <label className="font-bold">SSC Result*</label>
+              <input
+                {...register("ssc", { required: true })}
+                type="text"
+                placeholder="Your SSC Result: 5.00 "
+                className="input input-bordered w-full"
+              />
+            </div>
+
+            <div className=" space-y-2">
+              <label className="font-bold">HSC Result*</label>
+              <input
+                {...register("hsc", { required: true })}
+                type="text"
+                placeholder="Your HSC Result : 4.80"
+                className="input input-bordered w-full"
+              />
+            </div>
+          </div>
+
+          <div className="flex flex-col md:flex-row justify-between items-center">
+            <div className=" space-y-2">
+              <label className="font-bold">
+                Select The Appropirate Degree*
+              </label>
+              <br />
+              <select
+                {...register("degree", { required: true })}
+                className="select select-bordered w-full"
+              >
+                <option disabled defaultChecked>
+                  Select The Appropirate Degree
+                </option>
+                <option value="diploma">Diploma</option>
+                <option value="bachelor">Bachelor</option>
+                <option value="masters">Masters</option>
+              </select>
+            </div>
+
+            <div className=" space-y-2">
+              <label className="font-bold">Select Your Study Gap</label>
+              <br />
+              <select
+                {...register("studyGap")}
+                className="select select-bordered w-full"
+              >
+                <option disabled defaultValue>
+                  Select Your Study Gap
+                </option>
+                <option value="1 year">1 Year</option>
+                <option value="2 years">2 Year</option>
+                <option value="3 years or more">3 Years or More</option>
+              </select>
+            </div>
+          </div>
+
+          <div className=" space-y-2">
+            <label className="font-bold">University Name</label>
+            <input
+              disabled
+              {...register("universityName")}
+              type="text"
+              placeholder="Your HSC Result : 4.80"
+              className="input input-bordered w-full"
+            />
+          </div>
+          <div className=" space-y-2">
+            <label className="font-bold">Scholarship category</label>
+            <input
+              disabled
+              {...register("scholarshipCategory")}
+              type="text"
+              placeholder="Your HSC Result : 4.80"
+              className="input input-bordered w-full"
+            />
+          </div>
+          <div className=" space-y-2">
+            <label className="font-bold">Subject Category</label>
+            <input
+              disabled
+              {...register("subjectCategory")}
+              type="text"
+              placeholder="Your HSC Result : 4.80"
+              className="input input-bordered w-full"
+            />
+          </div>
+
+          <PrimaryButton text={"SUBMIT APPLICATION"}></PrimaryButton>
+        </form>
+      )}
+    </div>
   );
 };
 
